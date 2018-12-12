73e79a480897b77d5923fa138a9f85f256a7376a
z41z <just4iu@sina.com>
2018-12-12 17:18:52 +0800

    import sp-math

--- src/calcRadius.js
+++ src/calcRadius.js
@@ -1,3 +1,6 @@
+const {
+  pow
+} = require("sp-math");
 /**
  * 
  * @param {Number} tntWeight 
@@ -14,10 +17,10 @@ let radius = (tntWeight) => {
   let z2 = 1.95550803263784;
   let k = 5.6;
   let e = tntWeight * 4500;
-  let r0 = (Math.pow(tntWeight / 1000, 0.37) * 13.6).toFixed(2);
-  let r1 = (Math.pow(e * 1000 / p0, 1 / 3) * z1).toFixed(2);
-  let r2 = (Math.pow(e * 1000 / p0, 1 / 3) * z2).toFixed(2);
-  let r3 = (k * Math.pow(tntWeight, 1 / 3) / (Math.pow(1 + (3175 / tntWeight) * 2, 1 / 6))).toFixed(2);
+  let r0 = (pow(tntWeight / 1000, 0.37) * 13.6).toFixed(2);
+  let r1 = (pow(e * 1000 / p0, 1 / 3) * z1).toFixed(2);
+  let r2 = (pow(e * 1000 / p0, 1 / 3) * z2).toFixed(2);
+  let r3 = (k * pow(tntWeight, 1 / 3) / (pow(1 + (3175 / tntWeight) * 2, 1 / 6))).toFixed(2);
   return {
     r0,
     r1,
